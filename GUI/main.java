d65a2ba96512fceb6daa2fac8c0501db2d06793d
SHIVENDU TYAGI <88075377+shivendutyagi@users.noreply.github.com>
2022-10-11 11:30:31 +0530

    Update main.java

--- GUI/main.java
+++ GUI/main.java
@@ -1,1 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI;
 
+/**
+ *
+ * @author shivenduac
+ */
+public class main {
+    
+    public static void main(String[] args){
+        AppFrame frame = new AppFrame();
+    }
+    
+}
